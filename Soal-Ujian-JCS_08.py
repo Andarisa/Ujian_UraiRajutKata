907e94527a294d1f9ef3674f542b7c136805a71b
Andarisa <60954982+Andarisa@users.noreply.github.com>
2020-02-14 12:03:02 +0700

    Add files via upload

---
+++ Soal-Ujian-JCS_08.py
@@ -0,0 +1,29 @@
+# nomor 1
+
+def rajut(s):
+    for row in range(len(s)+1):
+        print(s[:row],end="")
+        
+rajut('code')
+rajut('python')
+rajut('Purwahdika')
+
+
+
+def urai(s):
+    f = len(set(s))*-1
+    print(s[f:])
+
+urai('ccocodcode')
+urai('ppypytpythpythopython')
+urai('PPuPurPurwPurwaPurwadPurwadhPurwadhiPurwadhikPurwadhika')
+
+
+
+
+
+
+ 
+
+
+
